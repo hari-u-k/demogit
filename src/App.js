86ee7bc2b21cbdf4ad95c6f771ff4ad106e5d7a3
hari-u-k <hariuk1206@gmail.com>
2023-04-21 10:53:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>Hi Hari, this is React...</h1>
+          <h2>Welcome to React...</h2>
         </p>
         <a
           className="App-link"
